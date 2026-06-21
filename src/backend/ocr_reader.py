55039547f7b0c7efc17b513286c19b8c2e134845
adelinamikki <adehaf2018@gmail.com>
2026-06-21 16:24:08 +0300

    Set Tesseract command path from environment variable

--- src/backend/ocr_reader.py
+++ src/backend/ocr_reader.py
@@ -1,5 +1,10 @@
 from PIL import Image
 import pytesseract
+import os
+
+tesseract_path = os.environ.get("TESSERACT_PATH")
+if tesseract_path:
+    pytesseract.pytesseract.tesseract_cmd = tesseract_path
 
 def extract_text(image_path):
     image = Image.open(image_path)
